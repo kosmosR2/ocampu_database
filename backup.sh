eed4c59a0adad73276259db3d5923c9087e673e0
kosmos on server <root@misasagi.me>
2015-06-09 12:09:46 +0000

    update

---
+++ backup.sh
@@ -0,0 +1,5 @@
+mongoexport -d nodejs -c scs -o scs
+mongoexport -d nodejs -c students -o students
+mongoexport -d nodejs -c teachers -o teachers
+mongoexport -d nodejs -c keys -o keys
+mongoexport -d nodejs -c courses -o courses
